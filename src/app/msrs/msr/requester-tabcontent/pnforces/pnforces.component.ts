21b02be3789f8ce296be59ad4a80c8f271f28644
Mike Chan <chanm0003@gmail.com>
2017-05-08 01:35:43 +0200

    Added fields for PN Forces

---
+++ src/app/msrs/msr/requester-tabcontent/pnforces/pnforces.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Msr, PNForce } from '../../../shared/msr.model';
+import * as _ from 'lodash';
+
+@Component({
+  selector: 'app-pnforces',
+  templateUrl: './pnforces.component.html',
+  styles: []
+})
+export class PnforcesComponent implements OnInit {
+  @Input() msr: Msr;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getButtonText() {
+    return (this.msr.PNForces.length === 0) ? 'Add a PN Force' : 'Add another PN Force';
+  }
+
+  addItem() {
+    this.msr.PNForces.push({
+      name: '',
+      number: 0,
+      parachuteType: ''
+    });
+  }
+
+  removeItem(index){
+    this.msr.PNForces.splice(index, 1);
+  }
+
+  shouldShow() {
+    if (this.msr.OperationType === 'AIR Mobility (SAM)') {
+      if (this.msr.AirMobilityType === "HAAR/TAAR") { return true; }
+      if (this.msr.AirMobilityType === "Equipment Drop") { return true; }
+      if (this.msr.AirMobilityType === "FARP") { return true; }
+      if (this.msr.AirMobilityType === "Infill/Exfill") {
+        return _.includes(['MFF', 'Static Line'], this.msr.InfillExfillType);
+      }
+    }
+    return false;
+  }
+
+  showParachuteType() {
+    if (this.msr.AirMobilityType === "Equipment Drop") { return true; }
+
+    if (this.msr.AirMobilityType === "Infill/Exfill") {
+        return _.includes(['MFF', 'Static Line'], this.msr.InfillExfillType);
+    }
+
+    return false;
+  }
+
+}
