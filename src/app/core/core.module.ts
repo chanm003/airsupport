d8c658899a3578a2757bc93d53a6adc75223992c
Mike Chan <mike.chan@kmdev.local>
2017-05-18 18:35:30 +0200

    Using HTTP GET against legacy SP2013 REST endpoint. Fixes #9

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -8,11 +8,13 @@ import { EntityService } from './entity.service';
 import { PeopleService } from './people.service';
 import { SimpleNotificationsModule } from 'angular2-notifications';
 import { LocalStorageModule } from 'angular-2-local-storage';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    HttpModule,
     SimpleNotificationsModule,
     LocalStorageModule.withConfig({
       prefix: 'msr-app',
@@ -20,7 +22,7 @@ import { LocalStorageModule } from 'angular-2-local-storage';
     })
   ],
   exports: [
-    CommonModule, FormsModule, SimpleNotificationsModule, LocalStorageModule
+    CommonModule, FormsModule, HttpModule, SimpleNotificationsModule, LocalStorageModule
   ],
   declarations: [],
   providers: [
--- src/app/core/people.service.ts
+++ src/app/core/people.service.ts
@@ -2,24 +2,37 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { PagecontextService } from '../core/pagecontext.service';
 import 'rxjs/add/observable/fromPromise';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class PeopleService {
-    constructor(private pagecontextService: PagecontextService) { }
+    constructor(private http: Http, private pagecontextService: PagecontextService) { }
 
     searchUsers = (text: string): any => {
-      return Observable.fromPromise(this.pagecontextService.getWeb().siteUsers
-        .select('Id', 'Title', 'Email')
-        .filter(`substringof('${text}',Title)`)
-        .orderBy('Title')
-        .get())
-        .map(data => {
-          const val = data.map(item => {
-            item.display = item.Title;
-            item.value = item.Id;
-            return item;
+      let fieldsToSelect = '';
+      if (environment.production) {
+        /*on-prem SP2013, uppercase 'M'*/
+        fieldsToSelect = 'Id,Name,WorkEMail';
+      } else {
+        /*cloud SP2013, lowercase 'm'*/
+        fieldsToSelect = 'Id,Name,WorkEmail';
+      }
+      const webUrl = this.pagecontextService.getInfo().currentWebAbsoluteUrl;
+      const url = `${webUrl}/_vti_bin/ListData.svc/UserInformationList?$select=${fieldsToSelect}&$filter=substringof('${text}',Name)`;
+      return this.http.get(url).map((resp: Response) => {
+        const results = resp.json().d.results;
+        const mappedData = results.map(item => {
+            /*make it look like data that comes back from /_api/web/siteusers*/
+            return {
+              Id: item.Id,
+              Title: item.Name,
+              Email: (item.WorkEmail) ? item.WorkEmail : item.WorkEMail,
+              display: item.Name,
+              value: item.Id
+            };
           });
-          return val;
-        });
+        return mappedData;
+      });
     }
 }
