0152ce5cf7a5aa9528021e74b0668a35c4586561
Mike Chan <mike.chan@kmdev.local>
2017-05-25 17:57:30 +0200

    Showing validation messages on child form

--- src/app/msrs/msr/requester-tabcontent/cargoform/cargoform.component.ts
+++ src/app/msrs/msr/requester-tabcontent/cargoform/cargoform.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, Output, EventEmitter, OnChanges } from '@angular/core';
 import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { Msr } from '../../../shared/msr.model';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-cargoform',
@@ -11,6 +12,7 @@ export class CargoformComponent implements OnChanges{
   @Input() msr: Msr;
   @Output() isValid = new EventEmitter();
   private cargoForm: FormGroup;
+  private validationMessages: any;
   constructor(private formBuilder: FormBuilder) {
     this.cargoForm = this.formBuilder.group({
      'NumberOfPAX': ['', Validators.required],
@@ -22,6 +24,34 @@ export class CargoformComponent implements OnChanges{
      'HazmatRequired': '',
      'AmplifyingDetail': ''
     });
+
+    this.validationMessages = {
+      'NumberOfPAX': {
+        required: 'Number of PAX is required'
+      }
+    };
+
+    this.cargoForm.valueChanges.subscribe(() => {
+      if (this.shouldShow()) {
+        this.isValid.emit(this.cargoForm.valid);
+      } else {
+        this.isValid.emit(false);
+      }
+    });
+  }
+
+  shouldShow() {
+    return this.msr.OperationType === 'Cargo/Personnel Move';
+  }
+
+  hasErrors(ctrlName) {
+    const ctrl = this.cargoForm.get(ctrlName);
+    return (ctrl.touched || ctrl.dirty) && !ctrl.valid;
+  }
+
+  getErrors(ctrlName) {
+    const ctrl = this.cargoForm.get(ctrlName);
+    return _.map(ctrl.errors, (value, key) => this.validationMessages[ctrlName][key]);
   }
 
   ngOnChanges() {}
