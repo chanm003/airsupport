90b801a88a51f8fd0430be6ddad897a33317dab0
Mike Chan <mike.chan@kmdev.local>
2017-06-30 18:05:52 +0200

    Added "Owned By" section to print-friendly view

--- src/app/msrs/print/print.component.ts
+++ src/app/msrs/print/print.component.ts
@@ -3,6 +3,7 @@ import { MsrService } from '../shared/msr.service';
 import { ActivatedRoute } from '@angular/router';
 import { MsrRouteData } from '../shared/msr-resolver.service';
 import { Msr } from '../shared/msr.model';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-print',
@@ -21,6 +22,7 @@ export class PrintComponent implements OnInit, OnDestroy {
   private sub: any;
   private msr: any;
   showFields = Msr.fieldsLogic;
+  cachedData: any;
 
   items = Array.from(Array(200), (_,x) => x);
 
@@ -28,18 +30,32 @@ export class PrintComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.route.data.subscribe((resolved: {data: MsrRouteData}) => {
       this.msr = resolved.data.msr;
+      this.cachedData = resolved.data.lookups;
+      this.msr.aieTypesDelimited = this.getAIETypes(this.msr);
+      const owningUnits = this.getOwningUnits(this.msr);
+      this.msr.owningUnitsDelimited = _.map(owningUnits, 'Name').join('; ');
+      this.msr.usersFromOwningUnits =
+        _.chain(owningUnits)
+          .map((item: any) => item.Users.results)
+          .flatten()
+          .uniqBy('Id')
+          .value();
     });
   }
 
   ngOnDestroy() {
   }
 
-  getAIETypes(msr) {
+  private getAIETypes(msr) {
     const types: Array<string> = [];
     if (msr.HoistRequired) { types.push('Hoist'); }
     if (msr.FastRopeRequired) { types.push('Fast Rope/FRIES'); }
     if (msr.RappelRequired) { types.push('Rappel'); }
     if (msr.OtherAIE) { types.push(msr.OtherAIE); }
     return types.join('; ');
+  }
+
+  getOwningUnits(msr) {
+    return _.map(msr.OwningUnitsId, (item) => _.find(this.cachedData.owningUnits, {Id: item}));
   }
 }
